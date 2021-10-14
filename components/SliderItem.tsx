6e2c96d817520d9c350e8cc041a9aa639f7580f6
William Lumbantobing <william.sl.tobing@gmail.com>
2021-10-14 19:28:27 +0700

    add dynamic slider

--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -1,16 +1,24 @@
 type sliderProps = {
   name: string;
   imgUrl: string;
+  idx: number;
 };
 
-const SliderItem = ({ name, imgUrl }: sliderProps) => {
+const SliderItem = ({ name, imgUrl, idx }: sliderProps) => {
+  const styleHandler = () => {
+    return { transform: `translateX(${-idx * 336}px)` };
+  };
+
   return (
     <>
-      <div className="relative mt-8 mr-4">
+      <div
+        style={styleHandler()}
+        className="relative mt-8 mr-4 flex-none duration-300 transform ease-in-out"
+      >
         <img
           src={imgUrl}
           alt="city_img"
-          className="rounded-md w-96 h-96 object-cover"
+          className="rounded-md w-80 h-96 object-cover"
         />
         <div className="absolute bottom-4 left-4 bg-white rounded px-2 py-1">
           <p className="text-black font-bold">{name}</p>
--- components/SliderList.tsx
+++ components/SliderList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import SliderItem from "./SliderItem";
 
@@ -20,26 +21,81 @@ const cities: Cities = [
     imgUrl:
       "https://web-busol.s3.ap-southeast-1.amazonaws.com/temp/Content_auto_x2_colored_toned_light_ai.webp",
   },
+  {
+    id: 3,
+    name: "YOGYAKARTA",
+    imgUrl:
+      "https://web-busol.s3.ap-southeast-1.amazonaws.com/temp/EOtDQfa6DmpNyMz43NCdWVvpo9Eg5BIARvdPKdEL.jpg",
+  },
+  {
+    id: 4,
+    name: "BALI",
+    imgUrl:
+      "https://web-busol.s3.ap-southeast-1.amazonaws.com/temp/Desktop.jpg",
+  },
+  {
+    id: 5,
+    name: "SEMARANG",
+    imgUrl:
+      "https://web-busol.s3.ap-southeast-1.amazonaws.com/temp/Content_auto_x2_colored_toned_light_ai.webp",
+  },
+  {
+    id: 6,
+    name: "YOGYAKARTA",
+    imgUrl:
+      "https://web-busol.s3.ap-southeast-1.amazonaws.com/temp/EOtDQfa6DmpNyMz43NCdWVvpo9Eg5BIARvdPKdEL.jpg",
+  },
 ];
 
 const Slider = () => {
+  const [idx, setIdx] = useState(0);
+
+  const leftClickHandler = () => {
+    if (idx - 1 >= 0) {
+      setIdx(idx - 1);
+    } else {
+      setIdx(cities.length - 1);
+    }
+  };
+
+  const rightClickHandler = () => {
+    if (idx + 1 <= cities.length - 1) {
+      setIdx(idx + 1);
+    } else {
+      setIdx(0);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center h-full text-white">
-      <div className="relative mb-4">
+      <div className="mb-4">
         <div className="flex overflow-hidden">
           {cities.map((city) => (
-            <SliderItem key={city.id} name={city.name} imgUrl={city.imgUrl} />
+            <SliderItem
+              key={city.id}
+              name={city.name}
+              imgUrl={city.imgUrl}
+              idx={idx}
+            />
           ))}
         </div>
       </div>
 
       <div className="flex">
-        <div className="border border-white hover:bg-white duration-300 hover:text-black rounded-md p-1 cursor-pointer mr-2">
+        <button
+          type="button"
+          onClick={leftClickHandler}
+          className="border border-white hover:bg-white duration-300 hover:text-black rounded-md p-1 mr-2"
+        >
           <ChevronLeftIcon className="h-5 w-5" />
-        </div>
-        <div className="border border-white hover:bg-white duration-300 hover:text-black rounded-md p-1 cursor-pointer">
+        </button>
+        <button
+          type="button"
+          onClick={rightClickHandler}
+          className="border border-white hover:bg-white duration-300 hover:text-black rounded-md p-1"
+        >
           <ChevronRightIcon className="h-5 w-5" />
-        </div>
+        </button>
       </div>
     </div>
   );
